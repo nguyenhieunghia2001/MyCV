0286d15279efca6cbd0719c96beff593198fa538
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-06-12 09:58:36 +0700

    home

---
+++ src/public/js/home.js
@@ -0,0 +1,10 @@
+const typed = new Typed(".element", {
+  strings: ["'m a Front-End developer", " love programming"],
+  smartBackspace: true, // Default value
+  loop: true,
+  startDelay: 400,
+  showCursor: true,
+  autoInsertCss: true,
+  typeSpeed: 65,
+  backSpeed: 23,
+});
